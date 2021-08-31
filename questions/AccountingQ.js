2a5857fd753e18e52719dd7954532cb2c8b164ef
Promicron <ileriodus@gmail.com>
2021-08-31 07:01:24 +0100

    first commit

---
+++ questions/AccountingQ.js
@@ -0,0 +1,71 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What standards are used to prepare financial statements by most of the countries and companies?",
+    answer: "International Financial Reporting Standards",
+    options: [
+      "International Financial Reporting Standards",
+      "International Financial Accounting Standards",
+      "International Accounting & Auditing Standards",
+      "International Risk Reporting Standards"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The correct form of Accounting equation is?",
+    answer: "Assets - Liabilities = Equity",
+    options: [
+      "Assets + Liabilities = Equity",
+      "Assets - Liabilities = Equity",
+      "Assets - Receivable = Equity",
+      "Assets + Receivable = Equity"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What from the following is NOT a non-current asset?",
+    answer: "Inventory",
+    options: [
+      "Capital",
+      "Property",
+      "Patent rights",
+      "Inventory"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The goods that have been sold to the customers are treated as _____ in the financial statements?",
+    answer: "Expenses",
+    options: [
+      "Inventories",
+      "Expenses",
+      "Income",
+      "Debt"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The selling price of some goods is $1500 and cost to sell the goods is $200. What is the Net Realizable Value (NRV)?",
+    answer: "$1000",
+    options: [
+      "$1000",
+      "$1200",
+      "$1300",
+      "$1500"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ questions/FootballQ.js
@@ -0,0 +1,71 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which player scored the fastest hat-trick in the Premier League?",
+    answer: "Sadio Mane",
+    options: [
+      "Alan Shearer",
+      "Sadio Mane",
+      "Wayne Ronney",
+      "Dieder Drogba"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these teams have won the World Cup twice?",
+    answer: "Uruguay",
+    options: [
+      "Argentina",
+      "England",
+      "Uruguay",
+      "Brazil"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of these German teams have won the champions league?",
+    answer: "Hamburg",
+    options: [
+      "Hamburg",
+      "Frankfurt",
+      "RB Leipzig",
+      "FC Cologne"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In what season did Roman Abramovich take over Chelsea?",
+    answer: "2003/04",
+    options: [
+      "2005/06",
+      "2003/04",
+      "2004/05",
+      "2002/03"
+    ]
+  },
+    {
+    numb: 5,
+    question: "'Estadio da Luz' stadium belongs to what club?",
+    answer: "Benfica Lisbon",
+    options: [
+      "Benfica Lisbon",
+      "Galatasaray",
+      "FC Porto",
+      "Valencia"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
